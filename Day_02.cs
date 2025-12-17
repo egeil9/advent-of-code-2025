f77bf0caf1e5707b041f344bc16173e912ea0951
Elizabeth Geil <geil.11@osu.edu>
2025-12-17 14:13:45 -0500

    day two problem one

--- Day_02.cs
+++ Day_02.cs
@@ -14,12 +14,55 @@ namespace advent_of_code_2025
 
         public Day_02()
         {
-            _input = File.ReadAllLines(InputFilePath);
+            _input = File.ReadAllText(InputFilePath).Split(",");
         }
 
-        public int Day_02_Solve_01()
+        public long Day_02_Solve_01()
         {
-            return -1;
+            long invalidTotal = 0;
+            long min;
+            long max;
+
+            foreach(var range in _input)
+            {
+                min = long.Parse(range.Substring(0, range.IndexOf("-")));
+                max = long.Parse(range.Substring(range.IndexOf("-") + 1));
+                List<long> potentialInvalidIds = new List<long>();
+
+                if(min.ToString().Length % 2 == 0)
+                {
+                    potentialInvalidIds = GetInvalidIds(min.ToString(), true, max);
+                }else if(max.ToString().Length % 2 == 0)
+                {
+                    potentialInvalidIds = GetInvalidIds(max.ToString(), false, min);
+                }
+                potentialInvalidIds.FindAll(id => id >= min && id <= max)?.ForEach(id => invalidTotal += id);
+            }
+            return invalidTotal;
+        }
+
+        public List<long> GetInvalidIds(string startingValue, bool add, long endOfRange)
+        {
+            List<long> invalidIds = new List<long>();
+            long sequence = long.Parse(startingValue.Substring(0, startingValue.Length / 2));
+            var inRange = true;
+            long id = 0;
+
+            while(inRange && sequence.ToString().Length == startingValue.Length / 2)
+            {
+                id = sequence * Convert.ToInt64(Math.Pow(10, startingValue.Length / 2)) + sequence;
+                if (add && id > endOfRange || !add && id < endOfRange)
+                {
+                    inRange = false;
+                }
+                else
+                {
+                    invalidIds.Add(id);
+                }
+                sequence = add ? sequence + 1 : sequence - 1;
+            }
+
+            return invalidIds;
         }
 
         public int Day_02_Solve_02()
